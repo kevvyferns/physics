0c15cae089dbecbf71d2e35b1a28a47e74ca7dd9
Kevin Fernandes <kevvyferns@gmail.com>
2018-10-18 23:33:25 +1300

    Updated the initial plotting file to show a sin^2 and a cos^3 function.

--- general/initial_plot.py
+++ general/initial_plot.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import json as js
 
 x = np.linspace(0, 2*np.pi, 100)
 y = np.sin(x)
 z = np.cos(x)
+a = z**2
+b = y**3
 
 Fig = plt.figure('Test fuctions')
 ax1 = Fig.add_subplot(111)
@@ -11,6 +14,8 @@ ax1.set_xlabel('Angle (rad)')
 ax1.set_ylabel('Amplitude (arb u.)')
 ax1.plot(x, y, 'g')
 ax1.plot(x, z, 'b')
+ax1.plot(x, a, 'r')
+ax1.plot(x, b, 'k')
 ax1.grid(True)
 ax1.autoscale(True)
 plt.show()
