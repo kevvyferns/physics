bc3d088a7781b4314b6b32e67f30503cbabab33c
Kevin Fernandes <kevvyferns@gmail.com>
2019-09-03 23:02:43 +1200

    Removed the previous import lines, and replaced them with a sys aided import line to allow import from other directories. This way we can now import useful_Functions.py from a directory outside the working directory.

--- general/initial_plot.py
+++ general/initial_plot.py
@@ -1,14 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import json as js
+import sys as ss
+ss.path.insert(1, '/Users/kevinfernandes/Documents/GitCodes_Projects/physics/optical')
+import useful_Functions as usf
 
-x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x)
-z = np.cos(x)
+x = usf.np.linspace(0, 2*usf.np.pi, 100)
+y = usf.np.sin(x)
+z = usf.np.cos(x)
 a = z**2
 b = y**3
 
-Fig = plt.figure('Test fuctions')
+Fig = usf.plt.figure('Test fuctions')
 ax1 = Fig.add_subplot(111)
 ax1.set_xlabel('Angle (rad)')
 ax1.set_ylabel('Amplitude (arb u.)')
@@ -18,4 +18,4 @@ ax1.plot(x, a, 'r')
 ax1.plot(x, b, 'k')
 ax1.grid(True)
 ax1.autoscale(True)
-plt.show()
+usf.plt.show()
