08d18f7405b325d6cf758052ca18df7454c54016
Kevin Fernandes <kevvyferns@gmail.com>
2019-09-03 22:27:02 +1200

    Added in exception handling into the useful_Functions.py file. The exception should be printed whenever the try/except block fails.

--- optical/useful_Functions.py
+++ optical/useful_Functions.py
@@ -22,24 +22,27 @@ def check_Dir(name, __path__):
             print ('File: {} created...'.format(name))
         else:
             print ('File: {} already exists...\nMoving on...'.format(name))
-    except:
+    except Exception as ckDirExec:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print ckDirExec
         pass
     return __path__ + name + '/'
     
 def make_Dir(val, _path_, _name__):
     try:
         os.makedirs(_path_ + _name__)
-    except:
+    except Exception as mkDirExec:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print mkDirExec
         pass
     
 def open_from_JSON(_path_, fname):
     try:
         with open(_path_ + fname) as f_:
             Data = js.load(f_)
-    except:
+    except Exception as opnJsnExec:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print opnJsnExec
         pass
     return Data
 
@@ -48,8 +51,9 @@ def save_to_JSON(filename, Data):
         with open(filename, 'w') as f:
             js.dump(Data, f, indent = 4, sort_keys = True)
         print ('Data saved in:\n\t{}'.format(filename))
-    except:
+    except Exception as savJsnExec:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print savJsnExec
         pass
     
 """- - - - - - - - - - - - - - - - - - - - - - - - - - - - -"""
@@ -63,8 +67,9 @@ def make_Stats(inList):
         mx = max(List)
         mn = min(List)
         out = {'Average' : ave, 'S.Dev' : std, 'Maximum' : mx, 'Minimum' : mn, 'Absolute Range' : np.ptp(List)}
-    except:
+    except Exception as statExc:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print statExc
         pass
     return out
 
@@ -75,8 +80,9 @@ def make_Fig(name, size = [12.5, 10]):
     try:
         # TODO: write function here
         print ('Nothing to see here...')
-    except:
+    except Exception as figExec:
         print ('Error in "{}" function'.format(ss._getframe().f_code.co_name))
+        print figExec
         pass
             
 """- - - - - - - - - - - - - - - - - - - - - - - - - - - - -"""
